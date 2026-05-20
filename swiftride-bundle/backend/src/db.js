9c631a00456529c6cd269feb76f90b5758c0815d
Vignesh-0319 <bhargava.vignesh@gmail.com>
2026-05-20 16:51:27 +0530

    Delete swiftride-bundle directory

--- swiftride-bundle/backend/src/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { env } from "./env.js";
-
-export async function connectDB() {
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(env.MONGO_URI, { autoIndex: true });
-  console.log("[db] connected");
-}
--- swiftride-bundle/backend/src/middleware/validate.js
+++
@@ -1,8 +0,0 @@
-export const validate = (schema, source = "body") => (req, res, next) => {
-  const result = schema.safeParse(req[source]);
-  if (!result.success) {
-    return res.status(400).json({ error: "Validation failed", issues: result.error.issues });
-  }
-  req[source] = result.data;
-  next();
-};
